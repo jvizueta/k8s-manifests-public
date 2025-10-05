f92b7b8e0aef25cb85433510ef32ab6d4b6edc2d
Juan Vizueta <jvizueta@gmail.com>
2025-10-05 11:26:03 +0100

    waha-integrator k8s manifests updated

---
+++ ai-customer-service/waha-integrator/overlays/dev/waha-integrator-secrets-sealer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script creates a sealed secret for the Waha Integrator application.
+# It uses kubectl and kubeseal to create a sealed secret that can be safely stored
+# in version control.
+
+# How to get your WAHA API Key: https://waha.devlike.pro/docs/how-to/security/
+WAHA_API_KEY="<your_waha_api_key>"
+
+# --scope cluster-wide (usable in any namespace/name — less restrictive; use with care)
+kubectl create secret generic waha-integrator-secrets\
+  --from-literal=WAHA_API_KEY="$WAHA_API_KEY" \
+  --dry-run=client -o json \
+| kubeseal --format yaml --scope cluster-wide \
+> waha-integrator-secrets.cluster.sealed.yaml
